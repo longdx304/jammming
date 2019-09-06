3f583aa05544f18eecf61bca75d4e9d4e90704b1
Long Do <longdo@Longs-MacBook-Air-2.local>
2019-09-06 16:57:51 +0700

    add static TrackList component

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TrackList.css';
+
+class TrackList extends React.Component {
+    render() {
+        return(
+            <div className="TrackList">
+                <h1>TrackList Component</h1>
+                {/* <!-- You will add a map method that renders a set of Track components  --> */}
+            </div>
+        );
+    }
+}
+
+export default TrackList;
