9a3d801570f8ec37ccffba1214859d7e1fcb0a08
Long Do <longdx@vfdcorp.com>
2019-09-06 18:59:19 +0700

    add key attribute to Track list items

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -8,7 +8,7 @@ class TrackList extends React.Component {
         return(
             <div className="TrackList">
                 {this.props.tracks && this.props.tracks.map(track => {
-                    return <Track track={track} />
+                    return <Track track={track} key={track.id} />
                 })}
             </div>
         );
