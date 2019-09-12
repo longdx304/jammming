37cf8896a3cf52c1dcef8faa6a65ad4e35f85ecd
Long Do <longdo@Longs-MacBook-Air-2.local>
2019-09-12 17:05:21 +0700

    add functionality to play samples

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,14 +4,28 @@ import './Track.css';
 class Track extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            play: false
+        }
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
+        this.tooglePlay = this.tooglePlay.bind(this);
     }
 
     renderAction(isRemoval) {
         return(
-            <button onClick={isRemoval ? this.removeTrack : this.addTrack} className="Track-action">{isRemoval ? '-' : '+'}</button>
+            <div>
+                <button onClick={this.tooglePlay}><i className={this.state.play ? "fas fa-pause" : "fas fa-play"}></i></button>
+                <button onClick={isRemoval ? this.removeTrack : this.addTrack} className="Track-action">{isRemoval ? '-' : '+'}</button>
+            </div>
         );
+    }
+
+    tooglePlay(event) {
+        const preview = document.getElementById(`preview_${this.props.track.id}`);
+        this.setState({play: !this.state.play}, () => {
+            this.state.play ? preview.play() : preview.pause();
+        });
     }
     
     addTrack(event) {
@@ -28,6 +42,7 @@ class Track extends React.Component {
                 <div className="Track-information">
                     <h3>{this.props.track.name}</h3>
                     <p>{this.props.track.artist} | {this.props.track.album}</p>
+                    <audio src={this.props.track.previewUrl} id={`preview_${this.props.track.id}`} />
                 </div>
                 {this.renderAction(this.props.isRemoval)}
             </div>
